1393c7f9d0bc5a149b0133258f92ca848d0362dd
Sriram K <sriram@lambdatest.com>
2025-12-08 12:15:29 -0600

    Initial commit: Node app

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello from LambdaTest sample app'));
+app.get('/api/echo', (req, res) => res.json({ echo: req.query.msg || 'hello' }));
+app.listen(port, () => console.log(`App running on http://localhost:${port}`));
