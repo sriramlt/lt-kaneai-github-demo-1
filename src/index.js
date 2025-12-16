338552fc8d4f1408b030cbc5f9e88d4c0d69f57b
Sriram K <sriram@lambdatest.com>
2025-12-16 08:00:11 -0600

    fix formatting

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from LambdaTest sample app'));
-app.get('/api/echo', (req, res) => res.json({ echo: req.query.msg || 'hello' }));
-app.listen(port, () => console.log(`App running on http://localhost:${port}`));
+app.get('/', (req, res) => 
+  res.send('Hello from LambdaTest sample app')
+);
+
+app.get('/api/echo', (req, res) =>
+  res.json({ echo: req.query.msg || 'hello' })
+);
+
+app.listen(port, () =>
+  console.log(`App running on http://localhost:${port}`)
+);
+
